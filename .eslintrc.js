c0ed74ed9fb7110386f1492edb1a70684feec5cd
nishantpal1911 <nishantpal3319@gmail.com>
2024-07-15 20:49:39 +0530

    Repo setup with eslint prettier and tailwind

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'react-app', 'react-app/jest', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'no-console': 'warn',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    eqeqeq: ['error', 'smart'],
+    'newline-before-return': 'error',
+    'prefer-destructuring': 'error',
+    'no-duplicate-imports': ['error', { includeExports: true }],
+    'import/newline-after-import': 'error',
+    'import/first': ['error', 'absolute-first'],
+    'import/order': ['error', { 'newlines-between': 'always' }],
+  },
+};
